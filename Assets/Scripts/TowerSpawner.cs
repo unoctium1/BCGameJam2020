a2070cd2d851ec087f71c638598cd98880cd4e7f
AbelW <unoctium1@gmail.com>
2020-02-16 13:47:43 -0800

    Finished setting up tower phase

--- Assets/Scripts/TowerSpawner.cs
+++ Assets/Scripts/TowerSpawner.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class TowerSpawner : MonoBehaviour
 {
+
+    public static TowerSpawner instance = null;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
     [SerializeField]
     private TowerBehavior[] towerPrefabs;
 
     public bool TrySpawn(int index)
     {
-        TowerBehavior tower = towerPrefabs[index];
+        TowerBehavior tower = instance.towerPrefabs[index];
 
         //test if can afford here
-
+        Debug.Log("Here");
         if(Game.instance.SelectedButton != null)
         {
             // A button is already selected ,spawn on the button
-
+            Debug.Log("button selected");
             Game.instance.SpawnTowerOnButton(tower);
         }
         else
         {
+            Debug.Log("mouse spawn");
             TowerBehavior go = Instantiate(tower);
             Game.instance.ActiveTowerToSpawn = go;
         }
@@ -28,6 +40,12 @@ public class TowerSpawner : MonoBehaviour
         return true;
 
     }
+
+    public void SpawnSimple() => TrySpawn(0);
+    public void SpawnChain() => TrySpawn(1);
+    public void SpawnRange() => TrySpawn(2);
+
+    public void SpawnCluster() => TrySpawn(3);
 
     private void Update()
     {
