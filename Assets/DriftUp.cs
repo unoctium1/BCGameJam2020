bf17cc05e27850f27540926b2268d3f3c921bb00
AbelW <unoctium1@gmail.com>
2020-02-16 14:41:38 -0800

    Added visualizers

---
+++ Assets/DriftUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriftUp : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.localPosition;
+        pos.y += speed * Time.deltaTime;
+        if(pos.y > 1.5)
+        {
+            pos.y = -1;
+        }
+        transform.localPosition = pos;
+    }
+}
---
+++ Assets/ScaleOnChannel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleOnChannel : MonoBehaviour
+{
+    [SerializeField]
+    AudioSpectrum spect;
+
+    [SerializeField]
+    int band;
+
+    [SerializeField]
+    float maxHeight;
+
+    Vector2 dimensions;
+
+    void Start()
+    {
+        dimensions = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dimensions.y = spect.PeakLevels[band] * maxHeight;
+        transform.localScale = dimensions;
+    }
+}
