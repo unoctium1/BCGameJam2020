f5827d10fee898b8066a3b05b129684fe5b153b9
AbelW <unoctium1@gmail.com>
2020-02-15 17:14:35 -0800

    Begin work on board

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+	[SerializeField]
+	int boardSize = 11;
+
+	[SerializeField]
+	GameBoard board = default(GameBoard);
+
+	void Awake()
+	{
+		board.Initialize(boardSize);
+	}
+}
---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour {
+
+	[SerializeField]
+	Transform ground = default(Transform);
+
+	int size;
+
+	public void Initialize(int size)
+	{
+		this.size = size;
+		ground.localScale = new Vector3(size, size, 1f);
+
+
+	}
+
+}
