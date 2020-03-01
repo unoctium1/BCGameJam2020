fe791d9d795befd76177989b7042896b57d492dc
AbelW <unoctium1@gmail.com>
2020-02-29 22:47:03 -0800

    Added depth testing scene

---
+++ Assets/Scripts/DepthAdjustment.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthAdjustment : MonoBehaviour {
+	
+	public void SetDepth(float depth)
+	{
+		if (depth < 0) depth = 0;
+		Vector3 pos = transform.localPosition;
+		pos.z = -depth;
+		transform.localPosition = pos;
+	}
+}
